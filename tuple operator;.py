5df8aa81e3d12cb17356f239645f9453e7b55352
Mankponse Augustin Gnanguenon <73544440+Augustin0527@users.noreply.github.com>
2026-03-07 09:43:44 -0500

    Add files via upload

---
+++ tuple operator;.py
@@ -0,0 +1,9 @@
+marks = [12,35,89,129,36]
+fruits = ["melon","apple","orange","raspeberry","mango"]
+print(" 1. accessing an element:",marks[2])
+print("2. slicing:",marks[1:5])
+print("3. lenght:",len(fruits))
+print("4.count:",fruits.count("apple"))
+print("5.index:",fruits.index("raspeberry")) 
+t3= marks + fruits
+print("6.concatenation :", t3)
