1b4f5082d4da33d32739bebaa4d56f637825abc8
Mankponse Augustin Gnanguenon <73544440+Augustin0527@users.noreply.github.com>
2026-04-16 17:46:39 -0400

    Add files via upload

---
+++ dog class.py
@@ -0,0 +1,11 @@
+class Student:
+   def __init__("race","name", "color""):
+       self.race = "race"
+       self.name = "name"
+       self.color = "color" 
+   def ("race")
+   ("hello my race is daltonian")
+   def ("name")
+   ("hello my name wuffy")
+   def ("color")
+   ("hello my color is black and white")
